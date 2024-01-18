61d116ff5b8ab9198930f4a6ba8003e241bf3061
vlcl753 <hello.git.vlcl@naver.com>
2024-01-18 17:35:45 +0900

    bank세팅

---
+++ lib/screen/main/tab/home/vo/banks_accounts_dummy.dart
@@ -0,0 +1,49 @@
+import 'package:fast_app_base/screen/main/tab/home/banks_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
+
+final bankAccountShinhan1 = BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+final bankAccountShinhan2 = BankAccount(bankShinhan, 30000000, accountTypeName: "저축예금");
+final bankAccountShinhan3 = BankAccount(bankShinhan, 300000000, accountTypeName: "저축예금");
+final bankAccountToss = BankAccount(bankTtoss, 5000000);
+final bankAccountKakao = BankAccount(bankKakao, 7000000, accountTypeName: "입출금통장");
+
+
+
+main(){
+  // print(bankAccounts[0].accountTypeName);
+  // for(final item in bankAccounts){
+  //   print(item.accountTypeName);
+  final shinhanBank = bankMap["shinhan1"];
+  // print(shinhanBank == bankAccountShinhan1);
+  for(final entry in bankMap.entries){ // Map을 순회하는 entries
+    print(entry.key + ":" + (entry.value.accountTypeName ?? entry.value.bank.name));  }
+}
+
+final bankMap = {
+  "shinhan1": bankAccountShinhan1,
+  "shinhan2": bankAccountShinhan2,
+};
+
+final bankAccounts = [
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao
+];
+
+
+final bankSet = {bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao};
